7df46ecc1123f5ef8a2499cc6cd24aec7ee95b7e
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-07-28 14:43:20 +0200

    updated project

--- src/components/countriesStats.js
+++ src/components/countriesStats.js
@@ -3,13 +3,23 @@ function CountriesStats(props){
         <div>
             <table>
                 <tr>
-                    <th>Country Names</th>
-                    <th>Active Cases</th>
+                    <th>
+                        Country Name
+                    </th>
+                    <th>
+                        Active Cases
+                    </th>
                 </tr>
+              {/* {props.countries.map((country) => (
+                <tr>
+                    <td>
+                        {country.Country}
+                    </td>
 
-               
+                   
+                </tr>
+              ))} */}
             </table>
-
         </div>
     )
 }
